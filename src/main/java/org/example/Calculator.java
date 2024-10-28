e423e51d480be8b832cf219e879d619a52fc4286
Mesfin Feleke <mesfindejene1313@gmail.com>
2024-10-28 18:51:17 -0500

    adding comments

--- src/main/java/org/example/Calculator.java
+++ src/main/java/org/example/Calculator.java
@@ -2,7 +2,12 @@ package org.example;
 
 
 public class Calculator {
-
+    /*
+     * Project Junit 5
+     * Created by: Mesfin D Feleke
+     * Date: 2024-10-23
+     * Description: Simple Junit 5 demo with new features
+     */
 
     public int doMath(int a, int b, MathsOperation op) {
         return op.operate(a, b);
